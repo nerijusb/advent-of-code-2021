0e291cc80ce17c2e17b501f97f78c71d881037ff
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-02 21:14:43 +0200

    Day 2 solved

--- Day02_1.java
+++ Day02_1.java
@@ -1,15 +1,69 @@
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 /**
  * Part one of
  * https://adventofcode.com/2021/day/2
  *
  * @author Nerijus
  */
-public class Day02_1 {
-    public static void main(String[] args) {
-        System.out.println("Result: " + new Day02_1().getResult());
+public class Day02_1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Horizontal position X final depth = " + new Day02_1().getResult());
     }
 
-    private int getResult() {
-        return -1;
+    private int getResult()
+    {
+        List<Instruction> instructions = readInstructions();
+
+        int horizontalPosition = 0;
+        int depth = 0;
+
+        for (Instruction instruction : instructions)
+        {
+            switch (instruction.direction)
+            {
+                case FORWARD -> horizontalPosition = horizontalPosition + instruction.amount;
+                case UP -> depth = depth - instruction.amount;
+                case DOWN -> depth = depth + instruction.amount;
+            }
+        }
+
+        return horizontalPosition * depth;
+    }
+
+    List<Instruction> readInstructions()
+    {
+        return Inputs.readStrings("Day02")
+                .stream()
+                .map(i -> {
+                    String[] parts = i.split(" ");
+                    InstructionDirection Instruction = InstructionDirection.valueOf(parts[0].toUpperCase(Locale.ROOT));
+                    int amount = Integer.parseInt(parts[1]);
+                    return new Instruction(Instruction, amount);
+                })
+                .collect(Collectors.toList());
+    }
+
+    static class Instruction
+    {
+        InstructionDirection direction;
+        int amount;
+
+        Instruction(InstructionDirection direction, int amount)
+        {
+            this.direction = direction;
+            this.amount = amount;
+        }
+    }
+
+    enum InstructionDirection
+    {
+        FORWARD,
+        UP,
+        DOWN
     }
 }
--- Day02_2.java
+++ Day02_2.java
@@ -1,15 +1,35 @@
+import java.util.List;
+
 /**
  * Part two of
  * https://adventofcode.com/2021/day/2
  *
  * @author Nerijus
  */
-public class Day02_2 extends Day01_1 {
+public class Day02_2 extends Day02_1 {
     public static void main(String[] args) {
-        System.out.println("Result: " + new Day02_2().getResult());
+        System.out.println("Horizontal position X final depth = " + new Day02_2().getResult());
     }
 
     private int getResult() {
-        return -1;
+        List<Instruction> instructions = readInstructions();
+
+        int horizontalPosition = 0;
+        int depth = 0;
+        int aim = 0;
+
+        for (Instruction instruction : instructions)
+        {
+            switch (instruction.direction)
+            {
+                case UP -> aim = aim - instruction.amount;
+                case DOWN -> aim = aim + instruction.amount;
+                case FORWARD -> {
+                    horizontalPosition = horizontalPosition + instruction.amount;
+                    depth = depth + aim * instruction.amount;
+                }
+            }
+        }
+        return horizontalPosition * depth;
     }
 }
