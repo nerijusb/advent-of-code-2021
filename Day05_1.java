4ed2958184f498d5a0f97bf789c950ec3c160cc4
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-04 11:46:25 +0200

    Day 5 placeholder

---
+++ Day05_1.java
@@ -0,0 +1,15 @@
+/**
+ * Part one of
+ * https://adventofcode.com/2021/day/5
+ *
+ * @author Nerijus
+ */
+public class Day05_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day05_1().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
