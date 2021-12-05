b64a5ab363f892959725e24b8ae2d9bfff01bbe8
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-05 11:10:33 +0200

    Day 6 placeholder

---
+++ Day06_1.java
@@ -0,0 +1,15 @@
+/**
+ * Part one of
+ * https://adventofcode.com/2021/day/6
+ *
+ * @author Nerijus
+ */
+public class Day06_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day06_1().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
---
+++ Day06_2.java
@@ -0,0 +1,15 @@
+/**
+ * Part two of
+ * https://adventofcode.com/2021/day/6
+ *
+ * @author Nerijus
+ */
+public class Day06_2 extends Day05_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day06_2().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
