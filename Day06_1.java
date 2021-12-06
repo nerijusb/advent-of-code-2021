d569a9d14fefff0ab10d87659be80ddf107d27b5
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-06 18:16:30 +0200

    Day 6 solved

--- Day06_1.java
+++ Day06_1.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Part one of
  * https://adventofcode.com/2021/day/6
@@ -5,11 +10,45 @@
  * @author Nerijus
  */
 public class Day06_1 {
+    private static final int DAY_COUNT = 80;
+
     public static void main(String[] args) {
-        System.out.println("" + new Day06_1().getResult());
+        System.out.println("Fish count: " + new Day06_1().getResult());
     }
 
     private int getResult() {
-        return -1;
+        List<Fish> allFish = Arrays.stream(Inputs.readString("Day06_demo").split(","))
+                .map(source -> new Fish(Integer.parseInt(source)))
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < DAY_COUNT; i++) {
+            List<Fish> addedFish = new ArrayList<>();
+            allFish.forEach(f -> {
+                Fish newFish = f.tick();
+                if (newFish != null) {
+                    addedFish.add(newFish);
+                }
+            });
+            allFish.addAll(addedFish);
+        }
+
+        return allFish.size();
+    }
+
+    static class Fish {
+        int timer;
+
+        public Fish(int timer) {
+            this.timer = timer;
+        }
+
+        Fish tick() {
+            if (timer == 0) {
+                timer = 6;
+                return new Fish(8);
+            }
+            timer = timer - 1;
+            return null;
+        }
     }
 }
--- Day06_2.java
+++ Day06_2.java
@@ -1,15 +1,64 @@
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Part two of
  * https://adventofcode.com/2021/day/6
  *
  * @author Nerijus
  */
-public class Day06_2 extends Day05_1 {
+public class Day06_2 extends Day06_1 {
+    private static final int DAY_COUNT = 256;
+
     public static void main(String[] args) {
-        System.out.println("" + new Day06_2().getResult());
+        System.out.println("Fish count: " + new Day06_2().getResult());
     }
 
-    private int getResult() {
-        return -1;
+    private String getResult() {
+        Map<Integer, BigDecimal> state = readInitialState();
+
+        for (int i = 0; i < DAY_COUNT; i++) {
+            Map<Integer, BigDecimal> newState = empty();
+            for (Integer days : state.keySet()) {
+                BigDecimal fishCount = state.get(days);
+                if (days == 0) {
+                    // changes to 6
+                    newState.put(6, newState.get(6).add(fishCount));
+                    // makes new
+                    newState.put(8, fishCount);
+                } else {
+                    // moves to another day timer
+                    newState.put(days - 1, newState.get(days - 1).add(fishCount));
+                }
+            }
+            state = newState;
+        }
+
+        return state.values().stream().reduce(BigDecimal::add).orElseGet(() -> new BigDecimal(-1)).toString();
+    }
+
+    private Map<Integer, BigDecimal> readInitialState() {
+        Map<Integer, BigDecimal> counts = empty();
+        Arrays.stream(Inputs.readString("Day06").split(","))
+                .map(Integer::parseInt)
+                .forEach(f -> counts.put(f, counts.get(f).add(new BigDecimal(1))));
+        return counts;
+    }
+
+    private HashMap<Integer, BigDecimal> empty() {
+        // day timer - fish count
+        return new HashMap<>(Map.of(
+                0, new BigDecimal(0),
+                1, new BigDecimal(0),
+                2, new BigDecimal(0),
+                3, new BigDecimal(0),
+                4, new BigDecimal(0),
+                5, new BigDecimal(0),
+                6, new BigDecimal(0),
+                7, new BigDecimal(0),
+                8, new BigDecimal(0)
+        ));
     }
 }
