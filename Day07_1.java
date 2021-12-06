778193907c147bc406424a37b9e9620ad0a44195
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-06 18:19:12 +0200

    Day 7 placeholder

---
+++ Day07_1.java
@@ -0,0 +1,15 @@
+/**
+ * Part one of
+ * https://adventofcode.com/2021/day/7
+ *
+ * @author Nerijus
+ */
+public class Day07_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day07_1().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
---
+++ Day07_2.java
@@ -0,0 +1,15 @@
+/**
+ * Part two of
+ * https://adventofcode.com/2021/day/7
+ *
+ * @author Nerijus
+ */
+public class Day07_2 extends Day07_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day07_2().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
