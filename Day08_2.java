43a1b076881e0f147e322befd3aabf21c5e81a72
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-07 19:08:10 +0200

    Day 8 placeholder

---
+++ Day08_2.java
@@ -0,0 +1,15 @@
+/**
+ * Part two of
+ * https://adventofcode.com/2021/day/7
+ *
+ * @author Nerijus
+ */
+public class Day08_2 extends Day08_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day08_2().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
