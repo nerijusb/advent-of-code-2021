a0bca4b1040552f45666af972e0d72bdb9dff577
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-07 19:05:49 +0200

    Day 7 solved

--- Day07_1.java
+++ Day07_1.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Part one of
  * https://adventofcode.com/2021/day/7
@@ -6,10 +11,28 @@
  */
 public class Day07_1 {
     public static void main(String[] args) {
-        System.out.println("" + new Day07_1().getResult());
+        System.out.println("Fuel spent to align to position: " + new Day07_1().getResult());
     }
 
     private int getResult() {
-        return -1;
+        List<Integer> depths = readInitialState();
+        return IntStream.range(min(depths), max(depths))
+                .map(i -> depths.stream().mapToInt(depth -> Math.abs(depth - i)).sum())
+                .min()
+                .orElse(-1);
+    }
+
+    int max(List<Integer> depths) {
+        return depths.stream().mapToInt(i -> i).max().orElseThrow();
+    }
+
+    int min(List<Integer> depths) {
+        return depths.stream().mapToInt(i -> i).min().orElseThrow();
+    }
+
+    List<Integer> readInitialState() {
+        return Arrays.stream(Inputs.readString("Day07").split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
--- Day07_2.java
+++ Day07_2.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.stream.IntStream;
+
 /**
  * Part two of
  * https://adventofcode.com/2021/day/7
@@ -6,10 +9,17 @@
  */
 public class Day07_2 extends Day07_1 {
     public static void main(String[] args) {
-        System.out.println("" + new Day07_2().getResult());
+        System.out.println("Fuel spent to align to position: " + new Day07_2().getResult());
     }
 
     private int getResult() {
-        return -1;
+        List<Integer> depths = readInitialState();
+        return IntStream.range(min(depths), max(depths))
+                .map(i -> depths.stream()
+                        .mapToInt(depth -> Math.abs(depth - i))
+                        .map(delta -> (delta * delta + delta) / 2)
+                        .sum())
+                .min()
+                .orElse(-1);
     }
 }
