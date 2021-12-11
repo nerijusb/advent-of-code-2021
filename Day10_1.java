8cd7ebb64d7e632bc7bdc63aadb3380fe756939f
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-11 09:43:53 +0200

    Day 10 placeholder

---
+++ Day10_1.java
@@ -0,0 +1,15 @@
+/**
+ * Part one of
+ * https://adventofcode.com/2021/day/10
+ *
+ * @author Nerijus
+ */
+public class Day10_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day10_1().getResult());
+    }
+
+    private long getResult() {
+        return -1;
+    }
+}
---
+++ Day10_2.java
@@ -0,0 +1,15 @@
+/**
+ * Part two of
+ * https://adventofcode.com/2021/day/10
+ *
+ * @author Nerijus
+ */
+public class Day10_2 extends Day10_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day10_2().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
