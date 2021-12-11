6108bd43c2945d320b48e4d5d31628e92e688e82
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-11 09:44:27 +0200

    Day 11 placeholder

---
+++ Day11_1.java
@@ -0,0 +1,15 @@
+/**
+ * Part one of
+ * https://adventofcode.com/2021/day/11
+ *
+ * @author Nerijus
+ */
+public class Day11_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day11_1().getResult());
+    }
+
+    private long getResult() {
+        return -1;
+    }
+}
---
+++ Day11_2.java
@@ -0,0 +1,15 @@
+/**
+ * Part two of
+ * https://adventofcode.com/2021/day/11
+ *
+ * @author Nerijus
+ */
+public class Day11_2 extends Day11_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day11_2().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
