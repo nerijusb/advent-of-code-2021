f17b2cb88111de11cfff9cbe99041f5cd3a2fa44
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-11 12:11:33 +0200

    Day 12 placeholder

---
+++ Day12_2.java
@@ -0,0 +1,15 @@
+/**
+ * Part two of
+ * https://adventofcode.com/2021/day/12
+ *
+ * @author Nerijus
+ */
+public class Day12_2 extends Day12_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day12_2().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
