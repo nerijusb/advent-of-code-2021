869fd1311e6d378bee392af6f5ba15bc33ff8fc4
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-13 20:44:15 +0200

    Day 14 placeholder

---
+++ Day14_1.java
@@ -0,0 +1,15 @@
+/**
+ * Part one of
+ * https://adventofcode.com/2021/day/14
+ *
+ * @author Nerijus
+ */
+public class Day14_1 {
+    public static void main(String[] args) {
+        System.out.println("" + new Day14_1().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
