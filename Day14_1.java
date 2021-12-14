fd227e8982d5f19fa467287e602ffdba93d348be
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-14 19:45:49 +0200

    Day 14-1 solved

--- Day14_1.java
+++ Day14_1.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * Part one of
  * https://adventofcode.com/2021/day/14
@@ -10,6 +13,45 @@ public class Day14_1 {
     }
 
     private int getResult() {
-        return -1;
+        String[] parts = Inputs.readString("Day14").split("\n\n");
+        String template = parts[0];
+        Map<String, String> instructions = readInstructions(parts[1]);
+        LinkedList<String> chain = new LinkedList<>(List.of(template.split("")));
+        LinkedList<String> newChain = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            while (!chain.isEmpty()) {
+                String first = chain.pop();
+                if (chain.isEmpty()) {
+                    newChain.addLast(first);
+                    break;
+                }
+                String second = chain.peek();
+                String output = instructions.get(first + second);
+                newChain.addLast(first);
+                if (output != null) {
+                    newChain.addLast(output);
+                } else {
+                    newChain.addLast(second);
+                }
+            }
+            newChain.forEach(System.out::print);
+            System.out.print("\n");
+            chain = newChain;
+            newChain = new LinkedList<>();
+        }
+
+        return calculateResult(chain);
+    }
+
+    private int calculateResult(LinkedList<String> chain) {
+        Set<String> letters = new HashSet<>(chain);
+        int max = letters.stream().mapToInt(l -> (int) chain.stream().filter(l::equals).count()).max().orElseThrow();
+        int min = letters.stream().mapToInt(l -> (int) chain.stream().filter(l::equals).count()).min().orElseThrow();
+        return max - min;
+    }
+
+    private Map<String, String> readInstructions(String instructions) {
+        return Arrays.stream(instructions.split("\n"))
+                .collect(Collectors.toMap(i -> i.split(" -> ")[0], i -> i.split(" -> ")[1]));
     }
 }
