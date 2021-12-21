1690c739a46232296ce8e9a7768daccc58ff4252
Nerijus <nerijus.bucinskis@gmail.com>
2021-12-21 18:44:16 +0200

    Day 21-1 solved

---
+++ Day21_1.java
@@ -0,0 +1,70 @@
+/**
+ * Part one of
+ * https://adventofcode.com/2021/day/21
+ *
+ * @author Nerijus
+ */
+public class Day21_1 {
+    // private static final Pattern INPUT_PATTERN = Pattern.compile("Player (?<player>\\d) starting position: (?<position>\\d)");
+
+    public static void main(String[] args) {
+        System.out.println("" + new Day21_1().getResult());
+    }
+
+    private int getResult() {
+        DeterministicDice dice = new DeterministicDice();
+        Player player1 = new Player(4);
+        Player player2 = new Player(7);
+
+        while (true) {
+            player1.move(dice.rollThree());
+            System.out.println("Player 1 moves to space " + player1.position + " for a total score of " + player1.score);
+            if (player1.score >= 1000) {
+                return dice.rollCount * player2.score;
+            }
+            player2.move(dice.rollThree());
+            System.out.println("Player 2 moves to space " + player2.position + " for a total score of " + player2.score);
+            if (player2.score >= 1000) {
+                return dice.rollCount * player1.score;
+            }
+        }
+    }
+
+    static class Player {
+        int position;
+        int score;
+
+        public Player(int position) {
+            this.position = position;
+        }
+
+        void move(int places) {
+            int newPosition = (this.position + places) % 10;
+            if (newPosition == 0) {
+                this.position = 10;
+            } else {
+                this.position = newPosition;
+            }
+            score = score + this.position;
+        }
+    }
+
+    static class DeterministicDice {
+        int current = 0;
+        int rollCount = 0;
+
+        int roll() {
+            rollCount++;
+            if (current == 100) {
+                current = 1;
+            } else {
+                current++;
+            }
+            return current;
+        }
+
+        int rollThree() {
+            return roll() + roll() + roll();
+        }
+    }
+}
